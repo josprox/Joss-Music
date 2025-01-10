ce3e11af9cf2a1cfdb3cad726bf140a6d9b9300a
josprox <joss@int.josprox.com>
2025-01-10 16:32:29 -0600

    Actualización de gradle a la versión 8.10.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.josprox.jossmusic"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "1.8.1"
+        versionCode = 25
+        versionName = "1.8.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
