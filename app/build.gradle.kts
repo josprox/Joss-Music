d1a4ee326a50072499c98747ad50ffa6c4cd23af
josprox <joss@int.josprox.com>
2025-02-07 14:10:16 -0600

    Corrección en el player, modificación de servicios en primer plano, cambios en el MusicService para un correcto funcionamiento y cambio en la estructura del MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.josprox.jossmusic"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.8.6"
+        versionCode = 30
+        versionName = "1.8.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
