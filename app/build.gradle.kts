ab92bcae4a0b214055494fd5b306b65a09e2c2cd
josprox <joss@int.josprox.com>
2025-02-10 13:21:15 -0600

    Se ha actualizado con las mejoras "innertube: always deobfuscate throttling parameter of streaming urls" de "Player fixes #1789"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.josprox.jossmusic"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.8.8"
+        versionCode = 31
+        versionName = "1.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
